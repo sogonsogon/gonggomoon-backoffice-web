649f8c160f02159ffe23ad5c24ec2334a22db9d7
seyun0714 <seun0714@naver.com>
2026-03-11 09:24:27 +0900

    feat: 공통 통신 로직 작성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import '@/app/globals.css';
+import QueryProvider from '@/shared/provider/QueryProvider';
 
 const pretendard = localFont({
   src: '../shared/assets/fonts/PretendardVariable.woff2',
@@ -21,7 +22,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko" className={`${pretendard.variable} ${pretendard.className}`}>
-      <body>{children}</body>
+      <QueryProvider>
+        <body>{children}</body>
+      </QueryProvider>
     </html>
   );
 }
