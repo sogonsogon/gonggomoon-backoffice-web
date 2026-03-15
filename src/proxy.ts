adab632cd3bfe324ac134d6820801527132fdb8a
seyun0714 <seun0714@naver.com>
2026-03-15 16:54:07 +0900

    chore: 내부 IP 우선 설정

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-const BASE_API_URL = process.env.NEXT_PUBLIC_API_URL;
+const BASE_API_URL = process.env.API_URL || process.env.NEXT_PUBLIC_API_URL;
 
 export async function proxy(request: NextRequest) {
   const accessToken = request.cookies.get('accessToken')?.value;
